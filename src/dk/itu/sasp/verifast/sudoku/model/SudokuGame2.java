4d5ce7476d512f5771c8332251dd9f1b610c3a7d
pkmetski <pkme@itu.dk>
2013-04-29 19:12:52 +0300

    Update SudokuGame2.java

--- src/dk/itu/sasp/verifast/sudoku/model/SudokuGame2.java
+++ src/dk/itu/sasp/verifast/sudoku/model/SudokuGame2.java
@@ -1,118 +1,164 @@
 package dk.itu.sasp.verifast.sudoku.model;
 
-//@ predicate validMove(SudokuGame2 sg, int x, int y, int number) = x>0 &*& y>0 &*& number >0;
-//@ predicate validNumber(int number) = number > 0 &*& number < 10; 
+import java.util.Arrays;
 
 public class SudokuGame2 {
-  
-  private int N;
-  private int[] game;
-  
-//@ predicate validSize() = N |-> ?n &*& game |-> ?g &*& g!=null;	
-  
-  public SudokuGame2(int N)
-  //@ requires N*N > 0;
-  //@ ensures this.N |-> ?n &*& this.game |-> ?g;
-  {
-        
-  	this.N = N;
-     	game = new int[N * N];
-     	
-  }
-  
-  public boolean isValidBoard()
-  //@ requires false;
-  //@ ensures true;
-  {
-    for(int i = 0; i < game.length; i++){
-      if(game[i] == 0) return true;
-      else return false;
-    }
-  }
-  
-  
-  void setNumber(int x, int y, int number)
-  //@ requires validSize() &*& validNumber(number) &*& array_slice(g,0,n*n,?elems);
-  //@ ensures validSize() &*& validNumber(number) &*& array_slice(g,0,n*n,elems);
-  {
-     int i = getIndex(x,y);
- 
-     if(i>0 && i<game.length){
-     
-        game[i] = number;
-     }
-  }
-  
-  int getIndex(int x, int y)
-  //@requires N |-> ?n; //&*& array_slice(g,0,n*n,?elems);
-  //@ensures N |-> n; // &*& array_slice(g,0,n*n,elems);
-  {
-  
-    return x + y * N;
-  
-  }
-  
 
-	private boolean isValidMove(int x, int y, int number)
-	//@ requires validSize() &*& validMove(this,_,_,number); //&*& x > -1 &*& x < n &*& y > -1 &*& y < n;
-	//@ ensures validSize() &*& validMove(this,_,_,number);
-	{
-		return isValidHorizontal(y, number) && isValidVertical(x, number)
-				&& isValidBlock(x, y, number);
-	}
+	private final int N = 9;
+	private int[] game;
 
-	private boolean isValidHorizontal(int y, int number)
-	//@ requires validSize() &*& validMove(this,_,_,number);//y > -1 &*& y < n &*& this.game |-> ?g;
-	//@ ensures validSize() &*& validMove(this,_,y,number);
-	{
-		for (int i = y * N; i < (y + 1) * N; i++)
-		//@ invariant N|-> n &*& i>= y*n &*& y > -1 &*& y < n &*& this.game |-> g;
-		{
-			if (game[i] == number) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private boolean isValidVertical(int x, int number)
-	//@ requires false;
+	public SudokuGame2()
+	// possible precondition - N <= sqrt(int.Max)
+	//@ requires N |-> ?n &*& n*n >0 &*& n>0;
 	//@ ensures true;
 	{
-		for (int i = x; i < game.length; i += N)
-		//@ invariant N|-> ?n &*& i>= x*n &*& y > -1 &*& y < n &*& this.game |-> g;
-		  {
-			if (game[i] == number) {
+		game = new int[N * N];
+	}
+
+	//Methods for determining if a move is valid	
+	private boolean isValidMove(int x, int y, int number)
+	//@ requires N |-> ?n &*& x > -1 &*& x < n &*& y > -1 &*& y < n;
+	//@ ensures true;
+	{
+		return isValidHorizontalMove(y, number)
+				&& isValidVerticalMove(x, number)
+				&& isValidBlockMove(x, y, number);
+	}
+
+	//Valid horizontal move
+	private boolean isValidHorizontalMove(int y, int number)
+	//@ requires N |-> ?n &*& y > -1 &*& y < n;
+	//@ ensures true;
+	{
+		//get the beginning of the row
+		int x = getIndex(0, y);
+		while ((x + 1) % N != 0)
+		//@ invariant true;
+		{
+			if (game[x] == number) {
 				return false;
 			}
+			x++;
 		}
 		return true;
 	}
 
-	private boolean isValidBlock(int x, int y, int number)
+	//Valid vertical move
+	private boolean isValidVerticalMove(int x, int number)
+	//@ requires N |-> ?n &*& x > -1 &*& x < n;
+	//@ ensures x > -1 &*& x < n;
+	{
+		//get the beginning of the column
+		int y = getIndex(x, 0);
+		while (y < N * N) {
+			if (game[y] == number) {
+				return false;
+			}
+			y += N;
+		}
+		return true;
+	}
+
+	//Valid block move
+	private boolean isValidBlockMove(int x, int y, int number)
 	//@ requires true;
 	//@ ensures true;
 	{
-		// 1. determine the block based on x and y
-		// 2. make sure the block has unique values from 1 to 9
+		int blockStartIndex = getBlockStartIndex(getIndex(x, y));
+		int[] blockIndicies = getBlockIndicies(blockStartIndex);
+		for (int i = 0; i < blockIndicies.length; i++) {
+			if (number == game[blockIndicies[i]]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	//Returns the index in one-dimensional array corresponding to x and y coordinates
+	private int getIndex(int x, int y) {
+		return x + y * N;
+	}
+
+	private int getBlockStartIndex(int index) {
+		// 1. get the correct column
+		index = index - (index % N);
+		// 2. get the correct row
+		index = index - (index % N * N * N);
+		return index;
+	}
+
+	private int[] getBlockIndicies(int blockStartIndex) {
+		int[] indicies = new int[N];
+		for (int i = 0; i < indicies.length; i++) {
+			indicies[i] = blockStartIndex;
+			for (int j = 0; j < Math.sqrt(N); j++) {
+				blockStartIndex++;
+			}
+			blockStartIndex += N;
+		}
+		return indicies;
+	}
+
+	//Methods for determining if a board is valid
+	private boolean isValidBoard(int[] game) {
+		return isValidHorizontalBoard(game) && isValidVerticalBoard(game)
+				&& isValidBlockBoard(game);
+	}
+
+	private boolean isValidHorizontalBoard(int[] game) {
+		int start = 0;
+		int end = 0;
+		while (end < N * N) {
+			end = start + N;
+			int[] row = Arrays.copyOfRange(game, start, end);
+			if (!uniqueNumbers(row)) {
+				return false;
+			}
+			start += N;
+		}
+		return true;
+	}
+
+	private boolean isValidVerticalBoard(int[] game) {
+		int[] column;
+		int x = 0;
+		int y = 0;
+		while (x < N) {
+			y = x;
+			column = new int[N];
+			for (int i = 0; i < N; i++) {
+				column[i] = game[y];
+				y += N;
+			}
+			if (!uniqueNumbers(column)) {
+				return false;
+			}
+			x++;
+		}
+		return true;
+	}
+
+	private boolean isValidBlockBoard(int[] game) {
+		// TODO Auto-generated method stub
+
+		//while start index < N * N
+		//increase startIndex by ?
+		//get the block indices
+		//for each block
+		//check if the array consists of unique numbers
 		return false;
 	}
-	*/
-}
 
-public class Program{
-
-public Program()
-//@ requires true;
-//@ ensures true;
-{}
-
-  public static void main(String[] args)
-  //@ requires true;
-  //@ ensures true;
-  {
-
-     SudokuGame2 s = new SudokuGame2(9);
-  }
-
+	//Returns true if the array has unique numbers in the range 1-9
+	//Zeroes are ignored
+	private boolean uniqueNumbers(int[] array) {
+		Arrays.sort(array);
+		for (int i = 0; i < N; i++) {
+			if (array[i % N] != 0 && array[(i + 1) % N] != 0
+					&& array[i % N] == array[(i + 1) % N]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
